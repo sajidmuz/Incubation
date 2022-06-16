67179cf5c445dc88e8e6a67fbbbae7c72edcb390
sajidmuz <92078177+sajidmuz@users.noreply.github.com>
2022-06-16 11:25:04 +0530

    Update Sort2DPoints.cpp
    Written Code Using oops

--- Sort2DPoints.cpp
+++ Sort2DPoints.cpp
@@ -41,13 +41,84 @@ Consider all possible test cases
 using namespace std;
 
 
+class Sort2DPoints
+{
+private:
+    map<double, vector<string>> mp;
+    
+
+    //Find distance from origin
+    double findDistance(double x, double y){return sqrt(pow(x, 2) + pow(y, 2));}
+
+    //Read file and put 2DPoints in a map
+    void readFile(string inputFile)
+    {
+        fstream inputFileStream;
+        inputFileStream.open(inputFile, ios::in);
+        if(inputFileStream.is_open()){
+            string line;        // Store one line at one iteration
+            string word;        // Store one co-ordinate at one iteraton
+            string distTostr;   // Store distance in string form 
+
+            double x = -1;      // X Co-ordiate
+            double y = 0;       // Y Co-ordinate
+            double dist = 0;    // Distance from origin to point
+            bool flag = true;
+
+            while (getline(inputFileStream, line)){             // Fetch one line at a time
+                stringstream ss(line);
+                while (ss >> word){
+                    if(flag){x = stod(word); flag=false;}         //Split lines into words 
+                    else{y = stoi(word);}                       // Put those words in X and Y variable
+                }
+        
+                dist = findDistance(x,y);                       //Find distance b/w origin and points
+                flag = true;                                    // For next iteration flag must be true
+                distTostr = to_string(dist); 
+                vector<string> res {distTostr,line};            //Put distance and points in vector
+                auto i = mp.find(dist);                         // Check key value for uniqueness
+                i == mp.end() ? mp.insert({dist, res}) : mp.insert({dist+0.001, res});
+                                                            // Insert Didtance and line in map
+            }
+            inputFileStream.close();
+        }
+    }
+  
+    //Write sorted data in a output file
+    void writeFile(string outputFile)
+    {
+        fstream outputFileStream;
+        outputFileStream.open(outputFile, ios::out);
+        if(outputFileStream.is_open()){
+            map<double, vector<string>>::iterator itr;
+            outputFileStream << "Distance" <<"\t\t"<< "Points" << endl;
+            outputFileStream << "--------" <<"\t\t"<< "------" << endl << endl;
+            for(itr=mp.begin(); itr!=mp.end(); ++itr){
+                outputFileStream << itr->second[0] <<"\t\t" << itr->second[1] <<endl;
+            }
+            outputFileStream.close();
+        }
+    }
+
+    public:
+
+    Sort2DPoints(){}
+    void getSorted2Points(string inputFile, string outputFile)
+    {
+        readFile(inputFile);
+        writeFile(outputFile);
+    }
+
+    //~Sort2DPoints();
+};
+
+
+
+
 int main()
 {
     string inputFile;
     string outputFile;
-    fstream inputFileStream;
-    fstream outputFileStream;
-    map<double, vector<string>> mp;
 
     cout << "Enter file path to read " << endl;
     cin >> inputFile;
@@ -56,46 +127,8 @@ int main()
     cout << "Enter file path to write " << endl;
     cin >> outputFile;
 
-    //Read file and put 2DPoints in a map
-    inputFileStream.open(inputFile, ios::in);
-    if(inputFileStream.is_open()){
-        string line;        // Store one line at one iteration
-        string word;        // Store one co-ordinate at one iteraton
-        string distTostr;   // Store distance in string form 
+    Sort2DPoints points;
+    points.getSorted2Points(inputFile, outputFile);
 
-        double x = -1;      // X Co-ordiate
-        double y = 0;       // Y Co-ordinate
-        double dist = 0;    // Distance from origin to point
-        
-        while (getline(inputFileStream, line)){             // Fetch one line at a time
-            stringstream ss(line);
-            while (ss >> word){                             //Split lines into words 
-                x == -1 ? x = stod(word) : y = stoi(word);  // Put those words in X and Y variable
-            }
-    
-            dist = sqrt(pow(x, 2) + pow(y, 2));             //Find distance b/w origin and points
-            x=-1;                                           // For next iteration reset value of x
-            distTostr = to_string(dist); 
-            vector<string> res {distTostr,line};            //Put distance and points in vector
-            auto i = mp.find(dist);                         // Check key value for uniqueness
-            i == mp.end() ? mp.insert({dist, res}) : mp.insert({dist+0.001, res});
-                                                        // Insert Didtance and line in map
-        }
-        inputFileStream.close();
-    }
-
-
-    //Write sorted data in a output file
-    outputFileStream.open(outputFile, ios::out);
-    if(outputFileStream.is_open()){
-        map<double, vector<string>>::iterator itr;
-        outputFileStream << "Distance" <<"\t\t"<< "Points" << endl;
-        outputFileStream << "--------" <<"\t\t"<< "------" << endl << endl;
-        for(itr=mp.begin(); itr!=mp.end(); ++itr){
-            outputFileStream << itr->second[0] <<"\t\t" << itr->second[1] <<endl;
-        }
-        outputFileStream.close();
-    }
-    
-   return 0;
+    return 0;
 }
